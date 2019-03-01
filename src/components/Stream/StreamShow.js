75b244cd189b486bab1029079fba2ce2039d2229
Swapnil Deshaware <deshaware.swapnil@gmail.com>
2019-03-01 21:14:19 +0530

    OBS Streaming

--- src/components/Stream/StreamShow.js
+++ src/components/Stream/StreamShow.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {fetchStream} from '../../actions';
 import Loading from '../Loading';
-import flv from 'flv.js';
+import flv from 'flv.js';//downloading stram and run on browser( like axios)
 
 class StreamShow extends React.Component{
     constructor(props){
@@ -13,19 +13,37 @@ class StreamShow extends React.Component{
     componentDidMount(){
         const {id} = this.props.match.params;
         console.log(this.videoRef)
-        this.props.fetchStream();
-        this.player = flv.createPlayer({
+        this.props.fetchStream(id);   
+        this.buildPlayer()  ;  
+    }
+
+    componentDidUpdate(){
+        this.buildPlayer();
+    }
+
+    buildPlayer(){
+        if(this.player || !this.props.stream)
+            return;
+        const {id} = this.props.match.params;
+         //this should be done only after having the actual stream
+         this.player = flv.createPlayer({
             type:'flv',
             url:`http://localhost:8000/live/${id}.flv`
         });
         this.player.attachMediaElement(this.videoRef.current);
         this.player.load()
+        this.player.play()
     }
+
+    componentWillUnmount(){
+        this.player.destroy();
+    }
+
     render(){
         if(!this.props.stream){
             return <Loading />;
         }
-        const {title,description} = this.props.stream
+        const {title,description} = this.props.stream;
         return(
             <div>       
                 <video ref={this.videoRef} style={{width:"100%"}} controls/>
